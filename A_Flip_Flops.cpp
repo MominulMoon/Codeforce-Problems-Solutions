bcf0994f34355be2b658c2f19b1091341300ab3c
MD Moon Babu <m.mmoon1527@gmail.com>
2026-05-09 16:12:17 +0600

    ✨ feat: add solutions for A_Cut_Ribbon, A_Flip_Flops, and A_Ilya_and_Bank_Account problems

---
+++ A_Flip_Flops.cpp
@@ -0,0 +1,120 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define iamspeed                      \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+#define FOR(i, j, n) for (int i = j; i < n; i++)
+#define trav(i, a)    \
+    for (auto &i : a) \
+        cin >> i;
+#define all(x) x.begin(), x.end()
+#define reverse_sort(v) sort(all(v), greater<int>())
+#define prnt(v)       \
+    for (auto &i : v) \
+        cout << i << " ";
+#define take(v)       \
+    for (auto &i : v) \
+    cin >> i
+#define ed "\n"
+#define PB push_back
+#define EB emplace_back
+#define MP make_pair
+#define F first
+#define S second
+#define GCD __gcd
+#define LCM(a, b) (a * b) / GCD(a, b)
+#define DEBUG(i) cout << "DEBUG " << i << "\n";
+#define sorted(x) is_sorted(x.begin(), x.end())
+#define BS(v, n) binary_search(v.begin(), v.end(), n)
+#define trans(x, y) transform(all(x), x.begin(), ::y)
+#define noo cout << "NO" << ed;
+#define yess cout << "YES" << ed;
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<ld> vld;
+typedef vector<ll> vll;
+
+const ll MOD = 998244353;
+const ll INF = 1e18;
+const ll NN = 1e6 + 1;
+// vll primes(NN+1,0);
+// void seive(){
+//     primes[0]=primes[1]=1;
+//     for(ll i=2;i*i<=NN;i++){
+//         if(primes[i]==0){
+//             for(ll j=i*i;j<=NN;j+=i){
+//                 primes[j]++;
+//             }
+//         }
+//     }
+// }
+
+void solve()
+{
+    ll n, c, k;
+    cin >> n >> c >> k;
+    vll v(n);
+    take(v);
+    sort(all(v));
+    FOR(i, 0, n)
+    {
+        if (v[i] > c)
+        {
+            break;
+        }
+        ll diff = c - v[i];
+        if (k >= diff)
+        {
+            c = c + v[i] + diff;
+            k -= diff;
+        }
+        else
+        {
+            c = v[i] + c + k;
+            k = 0;
+        }
+    }
+    cout << c << ed;
+}
+
+int main()
+{
+    iamspeed
+        // freopen("div7.in", "r", stdin);
+        // freopen("div7.out", "w", stdout);
+        // precompute();
+        int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
+/*
+                   ▄▄▄▄    ▄▄▄       ███▄    █  ██ ▄█▀▄▄▄       ██▓
+                  ▓█████▄ ▒████▄     ██ ▀█   █  ██▄█▒▒████▄    ▓██▒
+                  ▒██▒ ▄██▒██  ▀█▄  ▓██  ▀█ ██▒▓███▄░▒██  ▀█▄  ▒██▒
+                  ▒██░█▀  ░██▄▄▄▄██ ▓██▒  ▐▌██▒▓██ █▄░██▄▄▄▄██ ░██░
+                  ░▓█  ▀█▓ ▓█   ▓██▒▒██░   ▓██░▒██▒ █▄▓█   ▓██▒░██░
+                  ░▒▓███▀▒ ▒▒   ▓▒█░░ ▒░   ▒ ▒ ▒ ▒▒ ▓▒▒▒   ▓▒█░░▓
+                  ▒░▒   ░   ▒   ▒▒ ░░ ░░   ░ ▒░░ ░▒ ▒░ ▒   ▒▒ ░ ▒ ░
+                   ░    ░   ░   ▒      ░   ░ ░ ░ ░░ ░  ░   ▒    ▒ ░
+                   ░            ░  ░         ░ ░  ░        ░  ░ ░
+                        ░
+ ▄████▄   ▒█████  ▓█████▄ ▓█████      █████▒██▓ ███▄    █  ██▓  ██████  ██░ ██ ▓█████ ▓█████▄
+▒██▀ ▀█  ▒██▒  ██▒▒██▀ ██▌▓█   ▀    ▓██   ▒▓██▒ ██ ▀█   █ ▓██▒▒██    ▒ ▓██░ ██▒▓█   ▀ ▒██▀ ██▌
+▒▓█    ▄ ▒██░  ██▒░██   █▌▒███      ▒████ ░▒██▒▓██  ▀█ ██▒▒██▒░ ▓██▄   ▒██▀▀██░▒███   ░██   █▌
+▒▓▓▄ ▄██▒▒██   ██░░▓█▄   ▌▒▓█  ▄    ░▓█▒  ░░██░▓██▒  ▐▌██▒░██░  ▒   ██▒░▓█ ░██ ▒▓█  ▄ ░▓█▄   ▌
+▒ ▓███▀ ░░ ████▓▒░░▒████▓ ░▒████▒   ░▒█░   ░██░▒██░   ▓██░░██░▒██████▒▒░▓█▒░██▓░▒████▒░▒████▓
+░ ░▒ ▒  ░░ ▒░▒░▒░  ▒▒▓  ▒ ░░ ▒░ ░    ▒ ░   ░▓  ░ ▒░   ▒ ▒ ░▓  ▒ ▒▓▒ ▒ ░ ▒ ░░▒░▒░░ ▒░ ░ ▒▒▓  ▒
+  ░  ▒     ░ ▒ ▒░  ░ ▒  ▒  ░ ░  ░    ░      ▒ ░░ ░░   ░ ▒░ ▒ ░░ ░▒  ░ ░ ▒ ░▒░ ░ ░ ░  ░ ░ ▒  ▒
+░        ░ ░ ░ ▒   ░ ░  ░    ░       ░ ░    ▒ ░   ░   ░ ░  ▒ ░░  ░  ░   ░  ░░ ░   ░    ░ ░  ░
+░ ░          ░ ░     ░       ░  ░           ░           ░  ░        ░   ░  ░  ░   ░  ░   ░
+░                  ░                                                                   ░
+*/
